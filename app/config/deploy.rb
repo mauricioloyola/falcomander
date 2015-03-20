b71667e3e9f27eb6a8ebec2ec303029fdf1cd25b
Mauricio Loyola <mloyola@mbcomunicacionessac.com>
2015-03-20 13:41:28 -0500

    2003151341

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -6,17 +6,13 @@ set :app_path,    "app"
 set :user,      "usrlograrme"  
 set :use_sudo,      false
 ssh_options[:port] = 22123
-#set :php_bin,    "usr/bin/php" 
 
-#set :repository,  "https://mauricioloyola@bitbucket.org/mauricioloyola/frontend.git"
 set :repository,  "git@bitbucket.org:mauricioloyola/frontend.git"
-
 set :scm,         :git
 set :branch,        "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
-# Or: `propel`
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain
@@ -36,10 +32,3 @@ set :writable_dirs,       ["app/cache", "app/logs"]
 set :webserver_user,      "www-data"
 set :permission_method,   :acl
 set :use_set_permissions, true
-
-after "deploy:update_code" do
-  capifony_pretty_print "--> Fixing permissions"
-  run "cd #{latest_release} && find . -type f -exec chmod 644 {} \\"
-  run "cd #{latest_release} && find . -type d -exec chmod 755 {} \\"
-  capifony_puts_ok
-end
