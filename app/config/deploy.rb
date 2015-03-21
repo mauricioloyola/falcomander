9455ed4f65d5fee986e8f2e0591eff2fccee8d77
Mauricio Loyola <mloyola@mbcomunicacionessac.com>
2015-03-21 15:49:01 -0500

    2103151548

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,19 +3,17 @@ set :deploy_to,      "/var/www/vhosts/lograrme.com/symfony_projects/frontend/"
 set :domain,         "69.195.198.111"
 
 set :scm,            :git
-set :repository,     "https://mauricioloyola@bitbucket.org/mauricioloyola/frontend.git"
+set :repository,     "git@bitbucket.org:mauricioloyola/frontend.git"
 set :branch,         "master"
 
 role  :web,          domain
 role  :app,          domain, :primary => true
 
 set :user,           "usrlograrme"  
-set :password,       "40262378"  
 ssh_options[:port] = 22123
 set   :use_sudo,      false
 set   :keep_releases, 3
 
-#set :deploy_via, :rsync_with_remote_cache
 set :shared_files,      ["app/config/parameters.yml"]
 set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
 
@@ -26,3 +24,5 @@ set :model_manager, "doctrine"
 
 set :dump_assetic_assets, true
 set :interactive_mode, false
+
+
