8d638eb767affb26ec9d2ea5ccb08faf5779e5c9
Mauricio Loyola <mloyola@mbcomunicacionessac.com>
2015-03-21 13:34:20 -0500

    2103151334

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,15 +1,27 @@
-set :application, "Web frontend lograrme.com"
-set :deploy_to,   "/var/www/vhosts/lograrme.com/symfony_projects/frontend/"
-set :domain,      "69.195.198.111"
+set :application,    "Web frontend lograrme.com"
+set :deploy_to,      "/var/www/vhosts/lograrme.com/symfony_projects/frontend/"
+set :domain,         "69.195.198.111"
 
-set   :scm,       :git
-set :repository,  "git@bitbucket.org:mauricioloyola/frontend.git"
-set :branch,      "master"
+set :scm,            :git
+set :repository,     "https://mauricioloyola@bitbucket.org/mauricioloyola/frontend.git"
+set :branch,         "master"
 
-role  :web,       domain
-role  :app,       domain, :primary => true
+role  :web,          domain
+role  :app,          domain, :primary => true
 
-set :user,        "usrlograrme"  
+set :user,           "usrlograrme"  
 ssh_options[:port] = 22123
 set   :use_sudo,      false
 set   :keep_releases, 3
+
+set :deploy_via, :rsync_with_remote_cache
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+
+set :use_composer, true
+set :update_vendors, true
+
+set :model_manager, "doctrine"
+
+set :dump_assetic_assets, true
+set :interactive_mode, false
