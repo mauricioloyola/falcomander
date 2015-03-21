8cf104c85dc211b042090183cd6b9d4e88b2b75f
Mauricio Loyola <mloyola@mbcomunicacionessac.com>
2015-03-21 12:29:56 -0500

    2103151229

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -12,7 +12,8 @@ set :repository,  "git@bitbucket.org:mauricioloyola/frontend.git"
 set :scm,         :git
 set :branch,        "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
-set :deploy_via, :rsync_with_remote_cache
+
+set :deploy_via, :copy
 #set :deploy_via, :remote_cache
 
 #ssh_options[:forward_agent] = true
