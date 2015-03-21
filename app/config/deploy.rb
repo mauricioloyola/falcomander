405ce33d2ee45332534da8a26f573f5ad1e37f74
Mauricio Loyola <mloyola@mbcomunicacionessac.com>
2015-03-21 16:38:00 -0500

    2103151637

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,28 +1,26 @@
-set :application,    "Web frontend lograrme.com"
-set :deploy_to,      "/var/www/vhosts/lograrme.com/symfony_projects/frontend/"
-set :domain,         "69.195.198.111"
+set :application,   "Web frontend lograrme.com"
+set :domain,        "69.195.198.111"
+set :deploy_to,     "/var/www/vhosts/lograrme.com/symfony_projects/frontend/"
+set :app_path,      "app"
 
-set :scm,            :git
-set :repository,     "git@bitbucket.org:mauricioloyola/frontend.git"
-set :branch,         "master"
-
-role  :web,          domain
-role  :app,          domain, :primary => true
-
-set :user,           "usrlograrme"  
+set :user,          "usrlograrme"  
+set :use_sudo,      false
 ssh_options[:port] = 22123
-set   :use_sudo,      false
-set   :keep_releases, 3
 
-set :shared_files,      ["app/config/parameters.yml"]
-set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :repository,    "git@bitbucket.org:mauricioloyola/frontend.git"
+set :scm,           :git
+set :branch,        "master"
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain
+role :db,         domain, :primary => true       # This may be the same as your `Web` server
 
 set :use_composer, true
 set :update_vendors, true
 
-set :model_manager, "doctrine"
+set  :keep_releases,  3
 
-set :dump_assetic_assets, true
-set :interactive_mode, false
-
-
+set :shared_files,        ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
