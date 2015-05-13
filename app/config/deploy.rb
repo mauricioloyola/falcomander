5fa813468f8f701ab5d07044f7dce68e4787df20
Mauricio Loyola <mloyola@mbcomunicacionessac.com>
2015-05-12 22:34:59 -0500

    2105152234

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -18,7 +18,7 @@ role :app,        domain
 role :db,         domain, :primary => true       # This may be the same as your `Web` server
 
 set :use_composer, true
-#set :update_vendors, true
+set :update_vendors, true
 
 set  :keep_releases,  3
 
